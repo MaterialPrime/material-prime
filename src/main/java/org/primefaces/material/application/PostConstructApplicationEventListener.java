fe1d7690aebf3755b8bd7a4278902959fe0917c4
francesco-strazzullo <me@francescostrazzullo.info>
2015-01-20 22:39:02 +0100

    first commit

---
+++ src/main/java/org/primefaces/material/application/PostConstructApplicationEventListener.java
@@ -0,0 +1,19 @@
+package org.primefaces.material.application;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
+
+public class PostConstructApplicationEventListener implements SystemEventListener {
+
+	private static final Logger LOGGER = Logger.getLogger(PostConstructApplicationEventListener.class.getName());
+
+	public boolean isListenerForSource(final Object source) {
+		return true;
+	}
+
+	public void processEvent(final SystemEvent event) {
+		LOGGER.log(Level.INFO, "Running on MaterialPrime 0.0.1-SNAPSHOT");
+	}
+}
