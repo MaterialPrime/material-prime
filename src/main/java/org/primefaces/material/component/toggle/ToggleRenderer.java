17a00e1bd84478b3428e2703823fecbf74286aa6
francesco-strazzullo <me@francescostrazzullo.info>
2015-01-22 15:51:15 +0100

    managed value in toggle component

--- src/main/java/org/primefaces/material/component/toggle/ToggleRenderer.java
+++ src/main/java/org/primefaces/material/component/toggle/ToggleRenderer.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import javax.faces.convert.ConverterException;
 
+import org.primefaces.component.selectbooleancheckbox.SelectBooleanCheckboxRenderer;
 import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.ComponentUtils;
 
 public class ToggleRenderer extends CoreRenderer {
 	public static final String RENDERER_TYPE = "org.primefaces.material.component.ToggleRenderer";
@@ -20,6 +23,8 @@ public class ToggleRenderer extends CoreRenderer {
 
 	protected void encodeMarkup(FacesContext context, Toggle clock) throws IOException {
 		ResponseWriter writer = context.getResponseWriter();
+		
+		boolean checked = Boolean.valueOf(ComponentUtils.getValueToRender(context, clock));
 
 		writer.startElement("div", clock);
 			writer.writeAttribute("id", clock.getClientId(), null);
@@ -27,8 +32,14 @@ public class ToggleRenderer extends CoreRenderer {
 			writer.startElement("label", null);
 				writer.startElement("input", null);
 				writer.writeAttribute("type", "checkbox", null);
+				writer.writeAttribute("checked",  checked, null);
 				writer.endElement("input");
 			writer.endElement("label");	
 		writer.endElement("div");
+	}
+
+	@Override
+	public Object getConvertedValue(FacesContext context, UIComponent component, Object submittedValue) throws ConverterException {
+		return ((submittedValue instanceof Boolean) ? submittedValue : Boolean.valueOf(submittedValue.toString()));
 	}
 }
