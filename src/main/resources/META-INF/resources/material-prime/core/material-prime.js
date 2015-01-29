93f499f88bebe6200821c45fe36c37651f04f616
francesco-strazzullo <me@francescostrazzullo.info>
2015-01-29 22:25:25 +0100

    fixed widget namespace

--- src/main/resources/META-INF/resources/material-prime/core/material-prime.js
+++ src/main/resources/META-INF/resources/material-prime/core/material-prime.js
@@ -6,39 +6,40 @@
     }
     
     var MaterialPrime = {
-    	widgets:{},
-    	 cw : function(widgetConstructor, widgetVar, cfg, resource) {
-    		 MaterialPrime.createWidget(widgetConstructor, widgetVar, cfg, resource);
-         },
-         createWidget : function(widgetConstructor, widgetVar, cfg, resource) { 
-             if(MaterialPrime.widget[widgetConstructor]) {
-                 if(MaterialPrime.widgets[widgetVar]) {
-                	 MaterialPrime.widgets[widgetVar].refresh(cfg);                                     //ajax update
-                 }
-                 else {
-                	 MaterialPrime.widgets[widgetVar] = new MaterialPrime.widget[widgetConstructor](cfg);  //page init
-                     if(PrimeFaces.settings.legacyWidgetNamespace) {
-                         window[widgetVar] = MaterialPrime.widgets[widgetVar]; 
-                     }
-                 }
-             }
-             // widget script not loaded -> lazy load script + stylesheet
-             else {
-                 var scriptURI = PrimeFaces.getFacesResource(resource + '/' + resource + '.js', 'primefaces');
-                 var cssURI = PrimeFaces.getFacesResource(resource + '/' + resource + '.css', 'primefaces');
-
-                 //load css
-                 var cssResource = '<link type="text/css" rel="stylesheet" href="' + cssURI + '" />';      
-                 $('head').append(cssResource);
-
-                 //load script and initialize widget
-                 PrimeFaces.getScript(scriptURI, function() {
-                     setTimeout(function() {
-                    	 MaterialPrime.widgets[widgetVar] = new MaterialPrime.widget[widgetConstructor](cfg);
-                     }, 100);
-                 });
-             }
-         }
+		 widget:{},
+    	 widgets:{},
+		 cw : function(widgetConstructor, widgetVar, cfg, resource) {
+			 MaterialPrime.createWidget(widgetConstructor, widgetVar, cfg, resource);
+	     },
+	     createWidget : function(widgetConstructor, widgetVar, cfg, resource) { 
+	         if(MaterialPrime.widget[widgetConstructor]) {
+	             if(MaterialPrime.widgets[widgetVar]) {
+	            	 MaterialPrime.widgets[widgetVar].refresh(cfg);                                     //ajax update
+	             }
+	             else {
+	            	 MaterialPrime.widgets[widgetVar] = new MaterialPrime.widget[widgetConstructor](cfg);  //page init
+	                 if(PrimeFaces.settings.legacyWidgetNamespace) {
+	                     window[widgetVar] = MaterialPrime.widgets[widgetVar]; 
+	                 }
+	             }
+	         }
+	         // widget script not loaded -> lazy load script + stylesheet
+	         else {
+	             var scriptURI = PrimeFaces.getFacesResource(resource + '/' + resource + '.js', 'primefaces');
+	             var cssURI = PrimeFaces.getFacesResource(resource + '/' + resource + '.css', 'primefaces');
+	
+	             //load css
+	             var cssResource = '<link type="text/css" rel="stylesheet" href="' + cssURI + '" />';      
+	             $('head').append(cssResource);
+	
+	             //load script and initialize widget
+	             PrimeFaces.getScript(scriptURI, function() {
+	                 setTimeout(function() {
+	                	 MaterialPrime.widgets[widgetVar] = new MaterialPrime.widget[widgetConstructor](cfg);
+	                 }, 100);
+	             });
+	         }
+	     }
     };   
    
     window.MaterialPrime = MaterialPrime;
