533f5290b57605413f5773f900b1a471bdf3e563
francesco-strazzullo <me@francescostrazzullo.info>
2015-03-09 22:10:04 +0100

    created first draft of Input component

---
+++ src/main/java/org/primefaces/material/input/Input.java
@@ -0,0 +1,28 @@
+package org.primefaces.material.input;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+
+import org.primefaces.component.inputtext.InputText;
+import org.primefaces.material.MaterialPrime;
+
+@ResourceDependencies({
+	@ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
+	@ResourceDependency(library = "primefaces", name = "primefaces.js"),
+	@ResourceDependency(library = "material-prime", name = "libs/bootstrap.js"),
+	@ResourceDependency(library = "material-prime", name = "libs/bootstrap.css"),
+	@ResourceDependency(library = "material-prime", name = "libs/material.css"),
+	@ResourceDependency(library = "material-prime", name = "libs/material.js"),
+	@ResourceDependency(library = "material-prime", name = "libs/ripples.css"),
+	@ResourceDependency(library = "material-prime", name = "libs/ripples.js"),
+	@ResourceDependency(library = "material-prime", name = "core/material-prime.js"),
+	@ResourceDependency(library = "material-prime", name = "input/input.js")
+})
+public class Input extends InputText {
+	public static final String COMPONENT_TYPE = "org.primefaces.material.component.Input";
+	
+	@Override
+	public String getFamily() {
+		return MaterialPrime.COMPONENT_FAMILY;
+	}
+}
