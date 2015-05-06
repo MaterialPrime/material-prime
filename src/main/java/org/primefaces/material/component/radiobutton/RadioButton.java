2fd337ccac945982a93145c68f8cf37e3127cf6e
francesco-strazzullo <me@francescostrazzullo.info>
2015-05-06 17:44:13 +0200

    renamed resource directory

--- src/main/java/org/primefaces/material/component/radiobutton/RadioButton.java
+++ src/main/java/org/primefaces/material/component/radiobutton/RadioButton.java
@@ -12,14 +12,10 @@ import org.primefaces.material.MaterialPrime;
 @ResourceDependencies({
 	@ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
 	@ResourceDependency(library = "primefaces", name = "primefaces.js"),
-	@ResourceDependency(library = "material-prime", name = "libs/bootstrap.js"),
-	@ResourceDependency(library = "material-prime", name = "libs/bootstrap.css"),
-	@ResourceDependency(library = "material-prime", name = "libs/material.css"),
-	@ResourceDependency(library = "material-prime", name = "libs/material.js"),
-	@ResourceDependency(library = "material-prime", name = "libs/ripples.css"),
-	@ResourceDependency(library = "material-prime", name = "libs/ripples.js"),
+	@ResourceDependency(library = "material-prime", name = "libs/materialize.css"),
+	@ResourceDependency(library = "material-prime", name = "libs/materialize.js"),
 	@ResourceDependency(library = "material-prime", name = "core/material-prime.js"),
-	@ResourceDependency(library = "material-prime", name = "radioButton/radioButton.js")
+	@ResourceDependency(library = "material-prime", name = "radiobutton/radioButton.js")
 })
 public class RadioButton extends HtmlSelectOneRadio implements Widget{
 	public static final String COMPONENT_TYPE = "org.primefaces.material.component.RadioButton";
