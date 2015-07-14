330c49bef3c06ebdebf2a3a20a3d239a927c7ce6
Francesco Strazzullo <me@francescostrazzullo.info>
2015-07-14 21:57:47 +0200

    Draft of Range component

---
+++ src/main/java/org/primefaces/material/component/range/Range.java
@@ -0,0 +1,90 @@
+package org.primefaces.material.component.range;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIInput;
+import javax.faces.component.UINamingContainer;
+import javax.faces.component.behavior.ClientBehaviorHolder;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.IntegerConverter;
+
+import org.primefaces.component.api.Widget;
+import org.primefaces.material.MaterialPrime;
+
+@ResourceDependencies({
+	@ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
+	@ResourceDependency(library = "primefaces", name = "primefaces.js"),
+	@ResourceDependency(library = "material-prime", name = "libs/materialize.css"),
+	@ResourceDependency(library = "material-prime", name = "libs/materialize.js"),
+	@ResourceDependency(library = "material-prime", name = "core/material-prime.js"),
+	@ResourceDependency(library = "material-prime", name = "core/material-prime.css"),
+	@ResourceDependency(library = "material-prime", name = "range/range.js")
+})
+public class Range extends UIInput implements ClientBehaviorHolder,Widget{
+	public static final String COMPONENT_TYPE = "org.primefaces.material.component.Range";
+	
+	protected enum PropertyKeys {
+		min,
+		max,
+		disabled, widgetVar;
+	}
+	
+	public Range() {
+		setRendererType(RangeRenderer.RENDERER_TYPE);
+	}
+	
+	public int getMin(){
+		return (Integer) this.getStateHelper().eval(PropertyKeys.min, 0);
+	}
+	
+	public void setMin(int min){
+		this.getStateHelper().put(PropertyKeys.min, min);
+	}
+	
+	public int getMax(){
+		return (Integer) this.getStateHelper().eval(PropertyKeys.max, 0);
+	}
+	
+	public void setMax(int max){
+		this.getStateHelper().put(PropertyKeys.max, max);
+	}
+	
+	public boolean isDisabled() {
+        return (java.lang.Boolean) getStateHelper().eval(PropertyKeys.disabled, false);
+
+    }
+
+    public void setDisabled(boolean disabled) {
+        getStateHelper().put(PropertyKeys.disabled, disabled);
+    }
+    
+    public java.lang.String getWidgetVar() {
+		return (java.lang.String) getStateHelper().eval(PropertyKeys.widgetVar, null);
+	}
+    
+	public void setWidgetVar(java.lang.String _widgetVar) {
+		getStateHelper().put(PropertyKeys.widgetVar, _widgetVar);
+	}
+	
+	public String resolveWidgetVar() {
+		FacesContext context = getFacesContext();
+		String userWidgetVar = (String) getAttributes().get("widgetVar");
+
+		if(userWidgetVar != null)
+			return userWidgetVar;
+		 else
+			return "widget_" + getClientId(context).replaceAll("-|" + UINamingContainer.getSeparatorChar(context), "_");
+	}
+	
+	@Override
+	public Converter getConverter() {
+		return super.getConverter() != null ? super.getConverter() : new IntegerConverter(); 
+		
+	}
+	
+	@Override
+	public String getFamily() {
+		return MaterialPrime.COMPONENT_FAMILY;
+	}
+}
---
+++ src/main/java/org/primefaces/material/component/range/RangeRenderer.java
@@ -0,0 +1,77 @@
+package org.primefaces.material.component.range;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import org.primefaces.material.MaterialWidgetBuilder;
+import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.WidgetBuilder;
+
+public class RangeRenderer extends CoreRenderer {
+
+public static final String RENDERER_TYPE = "org.primefaces.material.component.CheckboxRenderer";
+	
+	@Override
+	public void decode(FacesContext context, UIComponent component) {
+		Range range = (Range) component;
+
+		if(range.isDisabled()) {
+			return;
+		}
+
+		decodeBehaviors(context, range);
+
+		String clientId = range.getClientId(context);
+		String submittedValue = (String) context.getExternalContext().getRequestParameterMap().get(clientId + "_input");
+		range.setSubmittedValue(submittedValue);
+	}
+	
+	@Override
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		Range checkbox = (Range) component;
+
+		encodeMarkup(context, checkbox);
+		encodeScript(context, checkbox);
+	}
+
+	private void encodeMarkup(FacesContext context, Range range) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		
+		String inputId = range.getClientId() + "_input";
+		
+		writer.startElement("p", range);		
+			writer.writeAttribute("id", range.getClientId(), null);
+			writer.writeAttribute("class", "range-field", null);
+			
+			writer.startElement("input", null);
+				writer.writeAttribute("id", inputId, null);
+				writer.writeAttribute("name", inputId, null);
+				writer.writeAttribute("value", range.getValue(), null);
+				writer.writeAttribute("min", range.getMin(), "min");
+				writer.writeAttribute("name", range.getMax(), "max");
+				if(range.isDisabled()){
+					writer.writeAttribute("disabled", range.isDisabled(), null);
+				}
+				writer.writeAttribute("type", "range", null);
+				
+			writer.endElement("input");				
+		writer.endElement("p");
+	}
+	
+	private void encodeScript(FacesContext context, Range checkbox) throws IOException {
+		String clientId = checkbox.getClientId();
+		String widgetVar = checkbox.resolveWidgetVar();
+		 
+		WidgetBuilder wb = MaterialWidgetBuilder.getInstance(context);
+		 
+		wb.initWithDomReady("Range", widgetVar, clientId);
+		encodeClientBehaviors(context, checkbox);
+		 
+		wb.finish();
+		 
+	}
+
+}
