c97671b3340836227beac14d513c55184070b755
francesco-strazzullo <me@francescostrazzullo.info>
2015-12-31 14:05:46 +0100

    changed version constant

--- src/main/java/org/primefaces/material/MaterialPrime.java
+++ src/main/java/org/primefaces/material/MaterialPrime.java
@@ -9,6 +9,6 @@ public class MaterialPrime {
 	
 	public static final String COMPONENT_FAMILY = "org.primefaces.material.component";
 	public static final Collection<String> MATERIAL_LOOKS = Collections.unmodifiableList(Arrays.asList(new String[]{"primary","info","success","warning","danger","link"}));
-	public static final String VERSION = "0.0.1-SNAPSHOT";	
+	public static final String VERSION = "0.0.2-SNAPSHOT";	
 	
 }
