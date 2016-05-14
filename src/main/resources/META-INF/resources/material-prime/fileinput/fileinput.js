25090e739aa7c000bc74d47f3dbba7ed8201ade5
Luca Cardinali <lucacard87@gmail.com>
2016-05-14 17:55:00 +0200

    fileinput

---
+++ src/main/resources/META-INF/resources/material-prime/fileinput/fileinput.js
@@ -0,0 +1,14 @@
+MaterialPrime.widget.FileInput = PrimeFaces.widget.BaseWidget.extend({
+	init : function(cfg) {
+		this._super(cfg);
+		this.input = jQuery(this.jqId+"_input");
+		var that = this;
+						
+		this.input.on("change",function(){		
+			if(that.cfg.behaviors && that.cfg.behaviors.valueChange) {				
+				that.cfg.behaviors.valueChange.call(that.input);
+			}
+		});		
+
+	}
+});
